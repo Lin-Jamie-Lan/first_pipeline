3bc63da528ab0e847a7b1e32e483654ff443586e
lin-jamie-lan <jamie.lin.lan@gmail.com>
2025-02-15 14:06:45 -0500

    update 2025-02-15

--- pipelines/pipeline1.py
+++ pipelines/pipeline1.py
@@ -4,13 +4,14 @@ from codebase.transformer import transform
 import pandas as pd
 from utils.file_utils import write_csv
 
-
 if __name__ == "__main__":
     config_path = "conf/config1.json"
     # create an object
-    job = FullReplaceJob(config_path)
+    # you call a class to create an instance (or object) of that class
+    job = FullReplaceJob(config_path) #need config_path, because def __init__(self, config_path) in the class
 
     # Run the FullReplaceJob to get the initial DataFrame
+    # If the function is a method inside a class, you need to call it on an instance of the class
     df = job.run()  # Assuming job.run() returns a DataFrame
 
     # Debug: Check if df is None
@@ -18,10 +19,10 @@ if __name__ == "__main__":
         raise ValueError("The DataFrame returned by job.run() is None. Check the FullReplaceJob implementation.")
     
     # Create an instance of the transformer and specify the id_column
-    transform = transform(config_path)
+    transforming = transform(config_path) #need config_path, because def __init__(self, config_path) in the class
 
     # Instantiate the transformer
-    marital_status_transformer = transform.maritalStatus(df, id_column='id')
+    marital_status_transformer = transforming.maritalStatus(df, id_column='id')
 
     # # Apply the transformer to the DataFrame
     # transformed_df = marital_status_transformer.execute(df)
